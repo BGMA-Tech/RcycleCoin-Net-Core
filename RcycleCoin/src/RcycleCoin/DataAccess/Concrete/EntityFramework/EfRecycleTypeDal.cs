6563d92f5b5423c3701c775e93197bdaf47002c7
mustafaalpyanikoglu <79158705+mustafaalpyanikoglu@users.noreply.github.com>
2022-12-05 18:59:34 +0300

    feat: ef is integrated

---
+++ RcycleCoin/src/RcycleCoin/DataAccess/Concrete/EntityFramework/EfRecycleTypeDal.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.Contexts;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfRecycleTypeDal : EfRepositoryBase<RecycleType, RecycleCoinContext>, IRecycleTypeDal
+    {
+        public EfRecycleTypeDal(RecycleCoinContext context) : base(context)
+        {
+        }
+    }
+}
